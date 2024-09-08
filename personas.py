d8ac7a23b326cd717931d738fd8d0e112a1769b0
Ricardo <rfmaciel.maciel@gmail.com>
2024-09-08 18:59:55 -0300

    se agrega la ocupacion a la persona

--- personas.py
+++ personas.py
@@ -1,7 +1,8 @@
 class Persona ():
-    def __init__(self,nombre, apellido, edad):
+    def __init__(self,nombre, apellido, edad, ocupacion):
         self.nombre=nombre
         self.apellido=apellido
         self.edad=edad
+        self.ocupacion=ocupacion
     def saluda (self):
         return f"hola, soy {self.nombre} {self.apellido}"
